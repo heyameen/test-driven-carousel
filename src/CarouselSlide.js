44d8e55819910aa956096b45fd944c57d11151ed
heyameen <aladeameen@gmail.com>
2022-01-05 15:42:25 +0100

    :sparkles: Initial implementation of CarouselSlide

---
+++ src/CarouselSlide.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CarouselSlide = ({ imgUrl, description, attribution, ...rest }) => (
+  <figure {...rest}>
+    <img src={imgUrl} />
+    <figcaption>
+        <strong>{description}</strong> {attribution}
+    </figcaption>
+  </figure>
+)
+
+export default CarouselSlide
--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -1,10 +1,7 @@
 import { configure, shallow } from 'enzyme' // 2
 
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import CarouselButton from '../CarouselButton'
 import React from 'react' // 1
-
-configure({ adapter: new Adapter() })
 
 describe('CarouselButton', () => {
   const text = 'Button text'
